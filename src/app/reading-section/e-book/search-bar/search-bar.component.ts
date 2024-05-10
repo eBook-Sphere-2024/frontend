3fdaacb1bf7e50e0f1b645afa321e52cd060d5f6
Eman Ramadan <eman01148597034@gmail.com>
2024-05-10 03:49:57 +0300

    edit search

--- src/app/reading-section/e-book/search-bar/search-bar.component.ts
+++ src/app/reading-section/e-book/search-bar/search-bar.component.ts
@@ -6,12 +6,13 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrl: './search-bar.component.css'
+  styleUrls: ['./search-bar.component.css']
 })
 export class SearchBarComponent implements OnInit {
   directsearchresults: eBookItem[] = [];
   indirectsearchresults: eBookItem[] = [];
   query: string = "";
+
   constructor(
     private eBookService: EBookService,
     private route: ActivatedRoute
@@ -20,26 +21,32 @@ export class SearchBarComponent implements OnInit {
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
       this.query = params['query'];
+      if (this.query) {
+        this.eBookService.get_directSearch(this.query).subscribe(
+          (data: any) => {
+            this.directsearchresults = data;
+          },
+          (error: any) => {
+            console.error('Error fetching direct search results:', error);
+          }
+        );
+        this.eBookService.get_InDirectSearch(this.query).subscribe(
+          (data: any) => {
+            for (let index = 0; index < data.length; index++) {
+              let indirectBook = data[index];
+              let found = this.directsearchresults.some(directBook => directBook.id === indirectBook.id);
+              if (!found) {
+                this.indirectsearchresults.push(indirectBook);
+              }
+            }
+            console.log("indirect", this.indirectsearchresults);
+            console.log("indirect length", this.indirectsearchresults.length);
+          },
+          (error: any) => {
+            console.error('Error fetching indirect search results:', error);
+          }
+        );
+      }
     });
-    if (this.query) {
-      this.eBookService.get_directSearch(this.query).subscribe(
-        (data: any) => {
-          this.directsearchresults = data;
-        },
-        (error: any) => {
-          console.error('Error fetching direct search results:', error);
-        }
-      );
-      this.eBookService.get_InDirectSearch(this.query).subscribe(
-        (data: any) => {
-          this.indirectsearchresults = data;
-          console.log("indirect", this.indirectsearchresults);
-          console.log("indirect length", this.indirectsearchresults.length);
-        },
-        (error: any) => {
-          console.error('Error fetching indirect search results:', error);
-        }
-      );
-    }
   }
 }
