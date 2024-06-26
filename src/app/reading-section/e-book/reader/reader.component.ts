e7171c37e552714edd48863be3710ebab3ad5a7a
yasmin <yasminahmed2643@gmail.com>
2024-06-26 10:54:08 +0300

    read url

--- src/app/reading-section/e-book/reader/reader.component.ts
+++ src/app/reading-section/e-book/reader/reader.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { EBookService } from '../e-book.service';
 import { EventService } from '../../../../shared/services/EventService';
 import { User } from '../../../../shared/models/User';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { UserServices } from '../../../User/user.service';
 
 @Component({
@@ -17,45 +17,42 @@ export class ReaderComponent implements OnInit, OnDestroy {
   totalPages: number = 0;
   currentPage: number = 1;
   highest_progress: number = 1;
-  private timer: any;
   showDialog: boolean = true; // Control variable for showing the dialog
+  showSignDialog: boolean = false;
 
-  constructor(private route: ActivatedRoute, private ebookService: EBookService,
+  constructor(private route: ActivatedRoute, private ebookService: EBookService, private router: Router,
     private events: EventService, private userService: UserServices) {
     this.ebookId = this.route.snapshot.paramMap.get('contentId');
     this.loadEbookContent();
   }
 
   ngOnDestroy(): void {
-    if (this.currentPage > this.highest_progress)
-      this.highest_progress = this.currentPage;
+    if (this.user != null) {
+      if (this.currentPage > this.highest_progress)
+        this.highest_progress = this.currentPage;
 
-    let dataPatch = {
-      "currentPgae": this.currentPage,
-      "highest_progress": this.highest_progress,
-      "totalPages": this.totalPages
-    };
+      let dataPatch = {
+        "currentPgae": this.currentPage,
+        "highest_progress": this.highest_progress,
+        "totalPages": this.totalPages
+      };
 
-    let dataPost = {
-      "user": this.user.id,
-      "ebook": this.ebookId,
-      "currentPgae": this.currentPage,
-      "highest_progress": this.highest_progress,
-      "totalPages": this.totalPages
-    };
+      let dataPost = {
+        "user": this.user.id,
+        "ebook": this.ebookId,
+        "currentPgae": this.currentPage,
+        "highest_progress": this.highest_progress,
+        "totalPages": this.totalPages
+      };
 
-    this.userService.updateReaderAnalysis(this.user.id, this.ebookId!, dataPost, dataPatch).subscribe(
-      (data: any) => {
-        console.log(data);
-      },
-      (error: any) => console.log(error)
-    );
+      this.userService.updateReaderAnalysis(this.user.id, this.ebookId!, dataPost, dataPatch).subscribe(
+        (data: any) => {
+          console.log(data);
+        },
+        (error: any) => console.log(error)
+      );
 
-    console.log("done");
-    // Clear the timer
-    if (this.timer) {
-      clearInterval(this.timer);
-      console.log('Timer stopped');
+      console.log("done");
     }
   }
 
@@ -66,26 +63,55 @@ export class ReaderComponent implements OnInit, OnDestroy {
         this.currentPage = data;
       });
     });
-
-    this.timer = setInterval(() => {
-      console.log('Timer is running');
-    }, 1000);
   }
 
   loadEbookContent(): void {
-    this.showDialog = true; // Show dialog when loading content
-    if (this.ebookId) {
-      this.ebookService.getEbookContent(this.ebookId).subscribe(
-        (res: any) => {
-          const blob = new Blob([res], { type: 'application/pdf' });
-          this.pdfSrc = URL.createObjectURL(blob);
-          console.log(this.pdfSrc);
-          this.showDialog = false; // Hide dialog when content is loaded
-        },
-        (error: any) => {
-          console.log(error);
-        }
-      );
+    if (this.user == null) {
+      const token = sessionStorage.getItem('Token');
+      if (token) {
+        this.userService.userProfile(token).subscribe(
+          (data: any) => {
+            this.user = data;
+            this.showDialog = true; // Show dialog when loading content
+            if (this.ebookId) {
+              this.ebookService.getEbookContent(this.ebookId).subscribe(
+                (res: any) => {
+                  const blob = new Blob([res], { type: 'application/pdf' });
+                  this.pdfSrc = URL.createObjectURL(blob);
+                  console.log(this.pdfSrc);
+                  this.showDialog = false; // Hide dialog when content is loaded
+                },
+                (error: any) => {
+                  this.router.navigate(['**'])
+                  console.log(error);
+                }
+              );
+            }
+          },
+          (error: any) => {
+            console.log(error.message);
+          }
+        );
+      } else {
+        this.showDialog = false;
+        this.showSignDialog = true;
+      }
+    } else {
+      this.showDialog = true; // Show dialog when loading content
+      if (this.ebookId) {
+        this.ebookService.getEbookContent(this.ebookId).subscribe(
+          (res: any) => {
+            const blob = new Blob([res], { type: 'application/pdf' });
+            this.pdfSrc = URL.createObjectURL(blob);
+            console.log(this.pdfSrc);
+            this.showDialog = false; // Hide dialog when content is loaded
+          },
+          (error: any) => {
+            this.router.navigate(['**'])
+            console.log(error);
+          }
+        );
+      }
     }
   }
 
@@ -104,5 +130,8 @@ export class ReaderComponent implements OnInit, OnDestroy {
     if (this.currentPage > this.highest_progress) {
       this.highest_progress = this.currentPage;
     }
+  }
+  goToSign() {
+    this.router.navigate(['/authentication']);
   }
 }
