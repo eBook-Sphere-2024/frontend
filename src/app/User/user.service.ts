8b08d0520cb9e654ef04ea23ccc4f4121f5f71ac
yasmin <yasminahmed2643@gmail.com>
2024-07-04 09:20:41 +0300

    Remove avatar

--- src/app/User/user.service.ts
+++ src/app/User/user.service.ts
@@ -105,6 +105,12 @@ export class UserServices {
       catchError(error => throwError(error))
     );
   }
+  removeAvatar(id: number) {
+    let options = this.getStandardOptions();
+    return this.http.delete('http://127.0.0.1:8000/api/profile/?id=' + id, options).pipe(
+      catchError(error => this.handleError(error, 'removeAvatar'))
+    );
+  }
   getFavoriteBooksForUser(id: number) {
     let options = this.getStandardOptions();
     return this.http.get('http://127.0.0.1:8000/api/FavoriteBooks/?user_id=' + id, options).pipe(
@@ -160,9 +166,9 @@ export class UserServices {
       catchError(error => this.handleError(error, 'updateReaderAnalysis (get)'))
     );
   }
-  BooksAnalysisNumbers(user_id:string,book_id: string) {
+  BooksAnalysisNumbers(user_id: string, book_id: string) {
     let options = this.getStandardOptions();
-    return this.http.get<any>('http://127.0.0.1:8000/api/BookAnalyticsNumbers/?author_id='+user_id+'&book_id='+book_id, options).pipe(
+    return this.http.get<any>('http://127.0.0.1:8000/api/BookAnalyticsNumbers/?author_id=' + user_id + '&book_id=' + book_id, options).pipe(
       catchError(error => this.handleError(error, 'BooksAnalysisNumbers'))
     );
   }
