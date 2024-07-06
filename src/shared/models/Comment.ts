dc52ce4af783cd461f3b5b7aafb8d7ad9d6f03a4
yasmin <yasminahmed2643@gmail.com>
2024-07-06 14:28:06 +0300

    remove likes from comment template

--- src/shared/models/Comment.ts
+++ src/shared/models/Comment.ts
@@ -1,8 +1,7 @@
 import { User } from "./User";
 import { eBookItem } from "./eBookItem";
 
-export class Comment{
-    constructor(public id: number, public user: User, 
-        public ebook: eBookItem, public content: string, public publish_date: Date,
-        public likes: number, public reply_to: Comment) { }
+export class Comment {
+    constructor(public id: number, public user: User,
+        public ebook: eBookItem, public content: string, public publish_date: Date, public reply_to: Comment) { }
 }
